704c3117afec789b953d670f345575043422df1e
paulo e gui <toi213521@gmail.com>
2025-05-30 10:37:16 -0300

    update

---
+++ cod.js
@@ -0,0 +1,15 @@
+btninciar = document.querySelector(".iniciar")
+btninciar.addEventListener("click", iniciarJogo)
+
+function iniciarJogo() {
+  telaInicial = document.querySelector(".tela-inicial")
+  telaInicial.style.display = "none"
+  
+  telapeg = document.querySelector(".tela-pergunta")
+  telapeg.style.display = "block"
+  
+  
+  
+  
+  
+}
