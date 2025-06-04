7d37e2b3d3be41f6a8716f98cadae96c36a9ce2f
toi213521@gmail.com <toi213521@gmail.com>
2025-06-04 08:30:34 -0300

    update

--- cod.js
+++ cod.js
@@ -1,3 +1,39 @@
+
+listaPerguntas = [
+{
+  pergunta: "Qual o maior ser vivo?",
+  opcoes : [
+    "Baleia azul",
+    "Fungo de Mel",
+    "Elefante",
+    "Girafa"
+  ],
+  correta: 1
+}, 
+{
+  pergunta: "Qual a primeira capital do Brasil",
+  opcoes : [
+    "Taquara",
+    "Brasilia",
+    "Salvador",
+    "Rio de Janeiro"
+  ],
+  correta: 2 
+},
+{
+  pergunta: "Em que ano o Brasil ganhou sua segunda Copa do Mundo",
+  opcoes : [
+    "1974",
+    "1970",
+    "1966",
+    "1962"
+  ],
+  correta: 3
+}
+]
+
+pergunta = null
+
 btninciar = document.querySelector(".iniciar")
 btninciar.addEventListener("click", iniciarJogo)
 
@@ -7,9 +43,12 @@ function iniciarJogo() {
   
   telapeg = document.querySelector(".tela-pergunta")
   telapeg.style.display = "block"
+
+mostraPergunta()
+}
+
+function mostraPergunta() {
+  pergunta = listaPerguntas[0]
   
-  
-  
-  
-  
+  console.log(pergunta.correta)
 }
